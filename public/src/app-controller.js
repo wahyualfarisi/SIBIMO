ac09d38b0ab1b35749ab76363fc6dfa113d6c427
wahyu alfarisi <wahyualfarisi@gmail.com>
2020-07-16 15:40:23 +0700

    finish crud jurusan

--- public/src/app-controller.js
+++ public/src/app-controller.js
@@ -103,4 +103,222 @@ const DashboardController = ( ( UI, AJAX ) => {
             load_dashboard();
         }
     }
-})(DashboardUI, ajaxSetting)
+})(DashboardUI, ajaxSetting);
+
+const JurusanController = ( (AJAX, LIB) => {
+
+
+    const EventListener = ( datatable ) => {
+
+        $('#t_jurusan').on('click', '.btn__edit', function() {
+            let id, nama;
+
+            id = $(this).data('id_jurusan');
+            nama = $(this).data('nama');
+
+            $('[name=id_jurusan]').val(id);
+            $('#jurusan').val(nama);
+            $('#modalEdit').modal('open');
+        });
+
+        $('#t_jurusan').on('click', '.btn__delete', function() {
+            let id_jurusan = $(this).data('id_jurusan');
+
+            if(id_jurusan){
+                $('[name=id_to_delete]').val(id_jurusan);
+                $('#modalDelete').modal('open');
+            }
+
+        })
+    }
+
+    const onSubmitFormAdd = ( table ) => {
+        $('#form_add').on('submit', function(e) {
+            e.preventDefault();
+            AJAX.postRes(
+                `/api/jurusan`,
+                this,
+                null,
+                res => {
+                    table.ajax.reload()
+                    $('#modalAdd').modal('close');
+                    M.toast({ html: 'Berhasil menambahkan jurusan' })
+                },
+                err => {
+                    console.log(err);
+                }
+            )
+        });
+    }
+
+    const onSubmitFormDelete = (table) => {
+        $('#form_delete').on('submit', function(e) {
+            e.preventDefault();
+            let id_jurusan = $('[name=id_to_delete]').val();
+
+            AJAX.deleteRes(
+                `/api/jurusan/${id_jurusan}`,
+                null,
+                res => {
+                    table.ajax.reload()
+                    $('#modalDelete').modal('close');
+                    M.toast({
+                        html: 'Berhasil Menghapus Jurusan'
+                    })
+                },
+                err => {
+                    console.log(err);
+                }
+            );
+        })
+    }
+
+    const onSubmitFormEdit = (table) => {
+        $('#form_edit').on('submit', function(e) {
+            e.preventDefault();
+            let id_jurusan = $('[name=id_jurusan]').val();
+
+            AJAX.putRes(
+                `/api/jurusan/${id_jurusan}`,
+                this,
+                null,
+                res => {
+                    table.ajax.reload()
+                    $('#modalEdit').modal('close');
+                    M.toast({
+                        html: 'Berhasil update jurusan'
+                    })
+                },
+                err => {
+                    console.log(err);
+                }
+            )
+
+        })
+    }
+
+    return {
+        data: () => {
+            console.log('data jurusan');
+            $('#modalEdit').modal();
+            $('#modalDelete').modal();
+            $('#modalAdd').modal();
+
+            let t_jurusan = $('#t_jurusan').DataTable({
+                processing: false,
+                language: AJAX.dtLanguage(),
+                dom: '<Bf<t>ip>',
+                pageLength: 50,
+                scrollY: 300,
+                scrollX: true,
+                buttons: {
+                    dom: {
+                        button: {
+                            tag: 'button',
+                            className: 'btn btn-small red darken-1 my-action'
+                        }
+                    },
+                    buttons: [
+                        {
+                            extend: 'collection',
+                            text: '<i class="material-icons dp48">file_download</i> ',
+                            buttons: [
+                                {
+                                    extend: 'pdfHtml5',
+                                    text: 'PDF',
+                                    exportOptions: {
+                                        columns: [1, 2, 3, 4, 5, 6]
+                                    },
+                                    filename: 'DATA_JURUSAN',
+                                    title: 'Data Jurusan'
+                                },
+                                {
+                                    extend: 'excelHtml5',
+                                    text: 'Excel',
+                                    exportOptions: {
+                                        columns: [1, 2, 3, 4, 5, 6]
+                                    },
+                                    filename: 'DATA_JURUSAN',
+                                    title: 'Data Jurusan'
+                                },
+                                {
+                                    extend: 'csvHtml5',
+                                    text: 'CSV',
+                                    exportOptions: {
+                                        columns: [1, 2, 3, 4, 5, 6]
+                                    },
+                                    filename: 'DATA_JURUSAN',
+                                    title: 'Data Jurusan'
+                                },
+                                {
+                                    extend: 'print',
+                                    text: 'Print',
+                                    exportOptions: {
+                                        columns: [1, 2, 3, 4, 5, 6]
+                                    },
+                                    filename: 'DATA_JURUSAN',
+                                    title: '<h4>Data Jurusan</h4>'
+                                },
+                            ]
+                        },
+                        {
+                            text: '<i class="material-icons dp48">autorenew</i>',
+                            action: function (e, dt, node, config) {
+                                dt.ajax.reload()
+                                M.toast({ html: 'Refresh table success' })
+                            },
+                        },
+                        {
+                            text: '<i class="material-icons dp48">add</i>',
+                            action: function (e, dt, node, config) {
+                                $('#modalAdd').modal('open');
+                            },
+                        },
+                    ]
+                },
+                ajax: AJAX.dtSettingSrc(
+                    `/api/jurusan`,
+                    {},
+                    res => {
+                        return res.results
+                    },
+                    err => {
+                        console.log(err)
+                    }
+                ),
+                columns: [
+                    {
+                        data: null,
+                        render: (data, type, row) => {
+                            return `<h6> ${row.nama_jurusan} </h6>`
+                        }
+                    },
+                    {
+                        data: null,
+                        render: (data, type, row) => {
+                            return moment(row.created_at).format("Y-M-D H:s")
+                        }
+                    },
+                    {
+                        data: null,
+                        render: (data, type, row) => {
+                            return `
+                            <a class="btn-floating mb-1 btn-flat waves-effect waves-light green white-text btn__edit" data-id_jurusan="${row.id_jurusan}" data-nama="${row.nama_jurusan}">
+                                <i class="material-icons">create</i>
+                            </a>
+                            <a class="btn-floating mb-1 btn-flat waves-effect waves-light red accent-2 white-text btn__delete" data-id_jurusan="${row.id_jurusan}">
+                                <i class="material-icons">close</i>
+                            </a>
+                            `   
+                        }
+                    }
+                ]
+            });
+            EventListener(t_jurusan);
+            onSubmitFormAdd(t_jurusan);
+            onSubmitFormDelete(t_jurusan);
+            onSubmitFormEdit(t_jurusan);
+            
+        }
+    }
+})(ajaxSetting, libSettings);
---
+++ resources/views/jurusan/data.blade.php
@@ -0,0 +1,102 @@
+<div class="row">
+    <div class="col s12">
+        <div class="container">
+            <div class="section section-data-tables">
+                <div class="row">
+
+                    <div class="col s12" id="table_container">
+                        <div class="card">
+                            <div class="card-content">
+
+                                <div class="row">
+
+                                    <div class="col s12">
+                                        <table id="t_jurusan" class="display nowrap" width="100%">
+                                            <thead>
+                                                <tr>
+                                                    <th>Jurusan</th>
+                                                    <th>Created at</th>
+                                                    <th>#</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                            </tbody>
+                                            <tfoot>
+                                                <tr>
+                                                    <th>Jurusan</th>
+                                                    <th>Created at</th>
+                                                    <th>#</th>
+                                                </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- -->
+
+<form id="form_add">
+        <div id="modalAdd" class="modal modal-fixed-footer">
+            <div class="modal-content">
+                <h4>Tambah Jurusan</h4>
+                    <div class="input-field col s12">
+                        <div>
+                            <label>Nama Jurusan</label>
+                            <input placeholder="Placeholder" id="nama_jurusan" name="nama_jurusan" type="text" class="validate" required>
+                        </div>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <a href="javascript:void(0)" class="modal-action modal-close waves-effect waves-green btn-flat ">BATAL</a>
+                <button type="submit" class="waves-effect waves-green btn-flat ">SUBMIT</button>
+            </div>
+        </div>
+</form>
+
+<form id="form_edit">
+    <div id="modalEdit" class="modal modal-fixed-footer">
+        <div class="modal-content">
+            <h4>Edit Jurusan</h4>
+            <input type="hidden" name="id_jurusan" />
+                <div class="input-field col s12">
+                    <div>
+                        <label>Nama Jurusan</label>
+                        <input placeholder="Placeholder" id="jurusan" name="nama_jurusan" type="text" class="validate" required>
+                    </div>
+                </div>
+        </div>
+        <div class="modal-footer">
+            <a href="javascript:void(0)" class="modal-action modal-close waves-effect waves-green btn-flat ">BATAL</a>
+            <button type="submit" class="waves-effect waves-green btn-flat ">EDIT</button>
+        </div>
+    </div>
+</form>
+
+<form id="form_delete">
+<div id="modalDelete" class="modal bottom-sheet">
+    <div class="modal-content">
+        <h4>Konfirmasi Delete</h4>
+        <input type="hidden" name="id_to_delete">
+    </div>
+    <div class="modal-footer">
+        <a href="javascript:void(0)" class="modal-action modal-close waves-effect waves-green btn-flat">BATAL</a>
+        <button type="submit" class="waves-effect waves-green btn-flat red darken-1 white-text ">DELETE</button>
+    </div>
+</div>
+</form>
+
+
+<script>
+    $(function() {
+        JurusanController.data();
+    })
+</script>
